cd4a013bcf2a83daaaa176a76f0f81042d4c8f5f
orrnacollection-sys <orrna.collection@gmail.com>
2026-06-16 14:00:24 +0530

    Fix CSP for Vercel login

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const securityHeaders = [
           key: "Content-Security-Policy",
           value: [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'strict-dynamic'",
+            "script-src 'self' 'unsafe-inline'",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com data:",
             "img-src 'self' data: blob: https://*.public.blob.vercel-storage.com",
